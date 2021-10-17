26063cc659a436b123e177f8ddc97c28232213cf
Michael Neis <michael.neis12@gmail.com>
2021-10-16 22:05:54 -0500

    commit tree e5db31246f85119f437f01da9a8a955cdb82f4d3

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 50) + 245
+
+
+let num3 = 12
+let num4 = 8
+const mod = num3 % num4
+
+
+const max = Math.max(12, 4, 16, 9, 2, 20)
